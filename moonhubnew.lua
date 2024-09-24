a48543d5c9f9d5ba2cfdbb27d6460fbe2a9e16c0
modevs93 <mohammadshwiki039@gmail.com>
2024-09-24 12:30:53 +0300

    Create moonhubnew.lua

---
+++ moonhubnew.lua
@@ -0,0 +1,26 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/REDzHUB/LibraryV2/main/redzLib")))()
+
+MakeWindow({
+  Hub = {
+    Title = "Moon HUB",
+    Animation = "by : Fearvamp"
+  },
+  Key = {
+    KeySystem = true,
+    Title = "Key System",
+    Description = "",
+    KeyLink = "",
+    Keys = {"Moonhubtop"},
+    Notifi = {
+      Notifications = true,
+      CorrectKey = "Running the Script...",
+      Incorrectkey = "The key is incorrect",
+      CopyKeyLink = "Copied to Clipboard"
+    }
+  }
+})
+
+local Main = MakeTab({Name = "Main"})
+
+local section = AddSection(Main, {"Teste"})
+
